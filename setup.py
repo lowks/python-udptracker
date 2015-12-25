5415c35ad62d1cf24f39f14de548ec3fde7936a0
Low Kian Seong <lowks@users.noreply.github.com>
2015-12-25 22:28:20 +0800

    Adding description and correcting url in setup.py
    Adding description and correcting url in setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     name='python-udptrack',
     packages=['udptrack'],
     version='0.0.5',
-    description='',
+    description='A Bittorrent udp-tracking protocol',
     long_description=Readme(),
     author='plasmashadow',
     author_email='plasmashadowx@gmail.com',
-    url='https://github.com/plasmashadow/python-udptrack.git',
-    classifiers=[
+    url='https://github.com/plasmashadow/python-udptrack',
+    A Bittorrent udp-tracking protocol classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Intended Audience :: Developers'
